30255548e530fcc3fd5995d45d6802ec010a0a1d
Carlos Sánchez <csanchezz@thinkandmake.net>
2018-03-27 00:22:25 -0600

    New GraphQl working

--- graph-user/schema/schema.js
+++ graph-user/schema/schema.js
@@ -1,10 +1,21 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
 
+const users = [
+    {id:'23', firstName:'Firulais', age:'304'},
+    {id:'101', firstName:'Maléfica', age:'1024'},
+    {id:'86', firstName:'Harmony', age:'21'},
+    {id:'13', firstName:'Zeus', age:'1332'},
+    {id:'35', firstName:'HolySpirit', age:'3333'},
+    {id:'58', firstName:'AlanTuring', age:'35'}
+    
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -23,8 +34,12 @@ const RootQuery = new GraphQLObjectType({
             type: UserType,
             args:{id:{type: GraphQLString}},
             resolve(parenValue, args){
-            
+                return _.find(users, { id: args.id});
             }
         }
     }
+});
+
+module.exports = new GraphQLSchema ({
+    query: RootQuery
 });
--- graph-user/server.js
+++ graph-user/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const expressGraphQl = require('express-graphql');
-
+const schema = require('./schema/schema');
 
 const app = express();
 
 app.use('/graphql', expressGraphQl({
+    schema,
     graphiql : true
 }))
 
