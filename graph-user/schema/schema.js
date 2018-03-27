2d66073555e8da9c1d2e3078ec39009c7efb69e0
Carlos Sánchez <csanchezz@thinkandmake.net>
2018-03-27 00:53:24 -0600

    second GraphQL

--- graph-user/schema/schema.js
+++ graph-user/schema/schema.js
@@ -8,38 +8,54 @@ const {
 } = graphql;
 
 const users = [
-    {id:'23', firstName:'Firulais', age:'304'},
-    {id:'101', firstName:'Maléfica', age:'1024'},
-    {id:'86', firstName:'Harmony', age:'21'},
-    {id:'13', firstName:'Zeus', age:'1332'},
-    {id:'35', firstName:'HolySpirit', age:'3333'},
-    {id:'58', firstName:'AlanTuring', age:'35'}
-    
+    { id: '23', firstName: 'Firulais', age: '304', message: 'Yoooh huuuu'},
+    { id: '101', firstName: 'Maléfica', age: '1024', message: 'A True Kiss'},
+    { id: '86', firstName: 'Harmony', age: '21', message: 'Music is my life'},
+    { id: '13', firstName: 'Zeus', age: '1332', message: 'Part in the valhala'},
+    { id: '35', firstName: 'HolySpirit', age: '3333', message: 'Full you soul'},
+    { id: '58', firstName: 'AlanTuring', age: '35', message: 'Creator of Computers' }
+
 ];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
     fields: {
-        id: { type:GraphQLString},
-        firstName: { type:GraphQLString},
-        age:{ type:GraphQLInt}
+        id: { type: GraphQLString },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt }
     }
-    
-}); 
+
+});
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
-        user:{
+        user: {
             type: UserType,
-            args:{id:{type: GraphQLString}},
-            resolve(parenValue, args){
-                return _.find(users, { id: args.id});
+            args: { id: { type: GraphQLString } },
+            resolve(parenValue, args) {
+                return _.find(users, { id: args.id });
             }
         }
     }
 });
 
-module.exports = new GraphQLSchema ({
-    query: RootQuery
+const AuthQuery = new GraphQLObjectType({
+    name: 'AuthQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: { id: { type: GraphQLString }, firstName: { type: GraphQLString } },
+            resolve(parenValue, args) {
+                return _.find(users, { id: args.id, firstName: args.firstName });
+            }
+        }
+    }
+});
+
+
+module.exports = new GraphQLSchema({
+    query: RootQuery,
+    authquery: AuthQuery
+    
 });
